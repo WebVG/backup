b0e5900e4587635732344dce7b0f73da3ad1b43c
Garrett <GVWebster.91@gmail.com>
2018-07-12 19:35:59 -0400

    Add files via upload

---
+++ loops/loops/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Garrett
+ * Date: 6/30/2014
+ * Time: 8:55 PM
+ * 
+ * loop an object create one first
+ */
+using System;
+
+namespace loops
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			/* can i make a pattern with the color
+			 *  */
+			
+			if(int x = 0; x < 1; x++)
+			{
+			Console.BackgroundColor = ConsoleColor.Red;
+				Console.WriteLine("  ");
+			}
+			else();
+			{
+				Console.BackgroundColor = ConsoleColor.Blue;
+				Console.WriteLine("	 ");
+			}
+			else();
+			{
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.WriteLine("	 ");
+			}
+		
+		}
+	}
+}
