cfbb31331e588567fb91f70af580292334505c90
Garrett <GVWebster.91@gmail.com>
2018-07-12 19:30:21 -0400

    Add files via upload

---
+++ Snow/Snow/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnowFall
+{
+    class SnowFall
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine();
+        }
+    }
+}
+/*
+ * second try. while loop program using an array.
+ * the program is snowfall
+*/
---
+++ Snow/Snow/snow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class snow
+{
+	public static void fall()  // the program is SnowFall, the method is fall
+	{
+
+	}
+}
